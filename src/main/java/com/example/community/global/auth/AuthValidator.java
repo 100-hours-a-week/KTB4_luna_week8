95c71c78b7320f96a731ed6af92b9c3adb47c310
LunaXion00 <lunaof12@gmail.com>
2026-07-07 19:34:49 +0900

    AllArgsConstructor 추가 및 deprecated method 제거

--- src/main/java/com/example/community/global/auth/AuthValidator.java
+++ src/main/java/com/example/community/global/auth/AuthValidator.java
@@ -2,31 +2,13 @@ package com.example.community.global.auth;
 
 import com.example.community.global.exceptions.ForbiddenException;
 import com.example.community.global.exceptions.UnauthorizedException;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@AllArgsConstructor
 public class AuthValidator {
-
-    public AuthValidator() {
-    }
-
-    @Transactional
-    public Long getLoginUserId(String authorizationHeader) {
-        if (authorizationHeader == null || authorizationHeader.isBlank() || !authorizationHeader.startsWith("Bearer ")) throw new UnauthorizedException();
-
-        String accessToken = authorizationHeader.substring("Bearer ".length());
-
-        return 1L;
-    }
-
-    @Transactional
-    public void validateOwner(String authorizationHeader, Long ownerId) {
-        Long loginUserId = getLoginUserId(authorizationHeader);
-
-        if (!loginUserId.equals(ownerId)) throw new ForbiddenException();
-
-    }
     public void validateOwner(Long ownerId, Long authorId) {
         if (!ownerId.equals(authorId)) throw new ForbiddenException();
     }
