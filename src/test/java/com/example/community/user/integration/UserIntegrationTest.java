d4bd655eada419f611be582d27450a1a9792ce3e
LunaXion00 <lunaof12@gmail.com>
2026-07-08 16:22:00 +0900

    application-test.properties 추가 적용

--- src/test/java/com/example/community/user/integration/UserIntegrationTest.java
+++ src/test/java/com/example/community/user/integration/UserIntegrationTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class UserIntegrationTest {
     @Autowired
     MockMvc mockMvc;
